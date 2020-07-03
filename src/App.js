44925d7a8800526fdc31422230f341177a824314
Thapelo Tsotetsi <me@tsotetsithapelo.co.za>
2020-07-03 16:16:04 +0200

    Add slider for the app.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { CardSlide } from 'react-card-slide/dist';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
+    <CardSlide items={[{
+          cardName: 'About Me',
+          cardDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          showBodyImage: true,
+          bodyImage: 'https://via.placeholder.com/950x350'
+        },{
+          cardName: 'Card Name 2',
+          cardDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          showBodyImage: true,
+          bodyImage: 'https://via.placeholder.com/950x350'
+        },{
+          cardName: 'Carde Name 3',
+          cardDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+          showBodyImage: true,
+          bodyImage: 'https://via.placeholder.com/950x350'
+        }]}/>
   );
 }
 
